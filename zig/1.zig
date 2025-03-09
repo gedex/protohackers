da98e576f5fbabead31ea87c4abdb40df155dbf3
Akeda Bagus <akeda.bagus@automattic.com>
2025-03-09 13:05:24 +0700

    Problem 1 solution in Zig

--- zig/1.zig
+++ zig/1.zig
@@ -4,6 +4,15 @@ const err = std.log.err;
 const indexOf = std.mem.indexOf;
 const Connection = std.net.Server.Connection;
 const Thread = std.Thread;
+const Allocator = std.mem.Allocator;
+const eql = std.mem.eql;
+const json = std.json;
+const BigInt = i256;
+
+const Request = struct {
+    method: []const u8,
+    number: BigInt,
+};
 
 pub fn usage(name: []u8) void {
     info("usage: {s} <host:port>", .{name});
@@ -31,7 +40,7 @@ pub fn main() !void {
 
     while (true) {
         if (server.accept()) |conn| {
-            const thread = try Thread.spawn(.{}, handleConn, .{conn});
+            const thread = try Thread.spawn(.{}, handleConn, .{allocator, conn});
             thread.detach();
         } else |e| {
             err("error accepting connection: {}", .{e});
@@ -39,21 +48,96 @@ pub fn main() !void {
     }
 }
 
-pub fn handleConn(conn: Connection) !void {
+pub fn handleConn(allocator: Allocator, conn: Connection) !void {
     info("{}> connection accepted", .{conn.address});
+
     defer {
-        conn.stream.close();
         info("{}> connection closed", .{conn.address});
+        conn.stream.close();
     }
 
-    var buffer: [4096]u8 = undefined;
+    var arr = std.ArrayList(u8).init(allocator);
+    defer arr.deinit();
+
     while (true) {
-        const bytes_received = try conn.stream.read(&buffer);
-        const chunk = buffer[0..bytes_received];
-        if (chunk.len == 0) {
-            info("{}> done writing", .{conn.address});
+        conn.stream.reader().streamUntilDelimiter(arr.writer(), '\n', null) catch |e| switch (e) {
+            error.EndOfStream => break,
+            else => unreachable,
+        };
+        info("{}> --> {s}", .{conn.address, arr.items});
+        const request = parseRequest(allocator, arr.items) catch {
+            try handleMalformedRequest(conn);
+            info("{} <-- malformed", .{conn.address});
             break;
+        };
+
+        var is_prime: bool = false;
+        if (request.number > 0) {
+            is_prime = isNumPrime(request.number);
         }
-        _ = try conn.stream.write(chunk);
+
+        info("{}> <-- prime: {}", .{conn.address, is_prime});
+        try handleConformingRequest(conn, is_prime);
+        arr.clearRetainingCapacity();
     }
+}
+
+fn parseRequest(allocator: Allocator, message: []u8) !Request {
+    var request: Request = undefined;
+
+    const parsed = try json.parseFromSlice(
+        json.Value,
+        allocator,
+        message,
+        .{ .ignore_unknown_fields = true },
+    );
+    defer parsed.deinit();
+
+    const method: ?json.Value = parsed.value.object.get("method");
+    const number: ?json.Value = parsed.value.object.get("number");
+    if (method == null or number == null) {
+        return error.ParseError;
+    }
+    switch (method.?) {
+        .string => {
+            if (!eql(u8, method.?.string, "isPrime")) {
+                return error.ParseError;
+            }
+            request.method = "isPrime";
+        },
+        else => return error.ParseError,
+    }
+    switch (number.?) {
+        .integer => request.number = number.?.integer,
+        .float => request.number = @intFromFloat(number.?.float),
+        .number_string => {
+            var big = try std.math.big.int.Managed.init(allocator);
+            defer big.deinit();
+
+            try big.setString(10, number.?.number_string);
+            request.number = try big.to(BigInt);
+        },
+        else => return error.ParseError,
+    }
+
+    return request;
+}
+
+fn handleMalformedRequest(conn: Connection) !void {
+    _ = try conn.stream.write("\n");
+}
+
+fn handleConformingRequest(conn: Connection, isPrime: bool) !void {
+    _ = try conn.stream.writer().print("{{\"method\":\"isPrime\",\"prime\":{}}}\n", .{isPrime});
+}
+
+fn isNumPrime(n: BigInt) bool {
+    if (n <= 1) return false;
+
+    var i: isize = 2;
+    while (i * i <= n) : (i += 1) {
+        if (@rem(n, i) == 0) return false;
+    }
+
+    return true;
 }
